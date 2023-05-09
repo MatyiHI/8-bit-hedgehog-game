a32f0546460edeec059cbb326b8dc77024574fed
Kreidler, Matyas Edgar <88744380+MatyiHI@users.noreply.github.com>
2023-05-09 11:02:56 -1000

    Added a song note thing

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
-info.onLifeZero(function () {
-    game.setGameOverMessage(true, "GAME OVER!")
-})
 info.setLife(3)
 scene.setBackgroundImage(assets.image`temp background`)
 controller.moveSprite(sprites.create(assets.image`Hedgehog main character`, SpriteKind.Player))
-music.play(music.createSong(hex`00780004080200`), music.PlaybackMode.UntilDone)
-sprites.create(assets.image`Hedgehog main character`, SpriteKind.Player).setStayInScreen(true)
+music.play(music.createSong(hex`00780004080f00`), music.PlaybackMode.UntilDone)
+let projectile = sprites.createProjectileFromSprite(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, sprites.create(assets.image`Hedgehog main character`, SpriteKind.Player), 50, 50)
