792e7513b41f99f21b1334750bda14d315435673
Kreidler, Matyas Edgar <88744380+MatyiHI@users.noreply.github.com>
2023-05-09 11:38:19 -1000

    Taco Tuesdays terrific code

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
 info.setLife(3)
-controller.moveSprite(sprites.create(assets.image`Hedgehog main character`, SpriteKind.Player))
+controller.moveSprite(sprites.create(assets.image`Hedgehog main character`, SpriteKind.Player), 100, 100)
 scene.setBackgroundImage(assets.image`temp background`)
 music.play(music.createSong(assets.song`Song`), music.PlaybackMode.UntilDone)
-let projectile = sprites.createProjectileFromSprite(assets.image`image`, sprites.create(assets.image`Evil hedgehog`, SpriteKind.Enemy), 50, 50)
+let projectile = sprites.createProjectileFromSprite(assets.image`image`, sprites.create(assets.image`Evil hedgehog`, SpriteKind.Enemy), 10, 50)
+if (sprites.create(assets.image`myImage0`, SpriteKind.Player).overlapsWith(sprites.create(assets.image`Evil hedgehog`, SpriteKind.Enemy))) {
+    sprites.create(assets.image`myImage0`, SpriteKind.Player).follow(sprites.create(assets.image`Evil hedgehog`, SpriteKind.Enemy))
+    sprites.destroy(sprites.create(assets.image`Evil hedgehog`, SpriteKind.Enemy))
+}
